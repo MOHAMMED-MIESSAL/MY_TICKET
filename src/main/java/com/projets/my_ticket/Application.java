20827a8a8896e3a5556967d77c974211dcfbe4d8
Mohammed Miessal <mohammedmiessal@gmail.com>
2024-12-29 18:07:49 +0100

    Créer et ajouter l'architecture au projet

--- src/main/java/com/projets/my_ticket/Application.java
+++ src/main/java/com/projets/my_ticket/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyTicketApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyTicketApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
