20a3819f41674dca387cbfe9bca56eb029518524
Mohammed Miessal <mohammedmiessal@gmail.com>
2025-01-28 15:02:30 +0100

    Add DTO User domain

--- src/main/java/com/projets/my_ticket/domain/User.java
+++ src/main/java/com/projets/my_ticket/domain/User.java
@@ -1,10 +1,11 @@
 package com.projets.my_ticket.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.projets.my_ticket.enums.Role;
 import com.projets.my_ticket.enums.Sexe;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
 
@@ -24,33 +24,28 @@ import java.util.UUID;
 @AllArgsConstructor
 public class User implements UserDetails {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @NotNull(message = "Username must not be null")
     @NotBlank(message = "Username must not be blank")
     private String username;
 
     @Column(unique = true)
     @NotBlank(message = "Email must not be blank")
-    @NotNull(message = "Email must not be null")
+    @Email(message = "Email must be valid")
     private String email;
 
     @NotBlank(message = "Password must not be blank")
-    @NotNull(message = "Password must not be null")
     private String password;
 
     @Enumerated(EnumType.STRING)
-    @NotNull(message = "Sexe must not be null")
     private Sexe sexe;
 
-    @NotNull(message = "Role must not be null")
     @Enumerated(EnumType.STRING)
     private Role role;
 
-
+    @JsonIgnore
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return role.getAuthorities()
@@ -58,22 +53,22 @@ public class User implements UserDetails {
                 .map(authority -> (GrantedAuthority) () -> authority)
                 .toList();
     }
-
+    @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
         return UserDetails.super.isAccountNonExpired();
     }
-
+    @JsonIgnore
     @Override
     public boolean isAccountNonLocked() {
         return UserDetails.super.isAccountNonLocked();
     }
-
+    @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired() {
         return UserDetails.super.isCredentialsNonExpired();
     }
-
+    @JsonIgnore
     @Override
     public boolean isEnabled() {
         return UserDetails.super.isEnabled();
